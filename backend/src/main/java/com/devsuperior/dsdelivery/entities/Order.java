a505799acac6513bc2148ed8b1825bcdcb591194
victtor hugo <hgloureirocosta@gmail.com>
2021-01-06 18:58:49 -0300

    Implementando a funcionalidade fazer pedido

--- backend/src/main/java/com/devsuperior/dsdelivery/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdelivery/entities/Order.java
@@ -38,6 +38,16 @@ public class Order implements Serializable{
 	public Order() {
 		
 	}
+	
+	public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.moment = moment;
+		this.status = status;
+	}
 
 	public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status,
 			Set<Product> products) {
