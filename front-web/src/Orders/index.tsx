5cd385098d67219f33baa9811f37a8fe4516b59f
victtor hugo <hgloureirocosta@gmail.com>
2021-01-08 21:52:21 -0300

    Adicionado card de produtos

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,5 +1,27 @@
+import './styles.css';
+import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../api';
+import { error } from 'console';
+
 function Orders(){
-   return( <div></div>);
+
+   const [products, setProducts] = useState<Product[]>([]);
+
+   useEffect(()=>{
+      fetchProducts()
+      .then(response => setProducts(response.data))
+      .catch(error => console.log(error))
+   },[])
+
+   return( 
+      <div className="orders-container">
+         <StepsHeader />
+         <ProductsList products={products}/>
+      </div>
+   );
 }
 
 export default Orders;
