761301d0dae11b74d1a611ce3d6041849a8abc39
victtor hugo <hgloureirocosta@gmail.com>
2021-01-13 00:30:48 -0300

    Finalizada front dos pedidos

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, Alert } from 'react-native';
+import { ScrollView, TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { fetchOrders } from '../api';
 import Header from '../Header';
 import OrderCard from '../OrderCard';
+import { Order } from '../types';
 
 function Orders() {
+
+   const [orders, setOrders] = useState<Order[]>([]);
+   const [isLoading, setIsLoading] = useState(false);
+
+   useEffect(()=> {
+      setIsLoading(true);
+      fetchOrders()
+         .then((response) => {
+            setOrders(response.data)
+         }).catch(() => {
+            Alert.alert("Houve um erro ao buscar os pedidos!");
+         })
+         .finally(() => setIsLoading(false));
+   }, []);
 
    const handleOnPress = () => {
    }
@@ -13,14 +29,15 @@ function Orders() {
       <>
          <Header />
          <ScrollView style={styles.container}>
-            <OrderCard />
-            <OrderCard />
-            <OrderCard />
-            <OrderCard />
-            <OrderCard />
-            <OrderCard />
-            <OrderCard />
-            <OrderCard />
+            {isLoading ? (
+               <Text> Buscando pedidos ...</Text>
+            ) : (
+               orders.map(order => {
+                  <TouchableWithoutFeedback key={order.id}>
+                     <OrderCard order={order}/>
+                  </TouchableWithoutFeedback>
+               })
+            )}
          </ScrollView>
       </>
    );
